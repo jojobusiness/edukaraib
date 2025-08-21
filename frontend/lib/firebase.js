62e02046a0ae220c0f503b96adaf67426b7275e3
Jojobusiness <johnfres.45@gmail.com>
2025-08-21 04:28:54 +0200

    Upgrade du site

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDoPTDEtgcROB-PkLehddqr3Lpy_nM5P4A",
@@ -11,6 +12,8 @@ const firebaseConfig = {
   appId: "1:827164038836:web:8f0ce9776e18d1b03da9e1",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
