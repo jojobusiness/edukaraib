d5fec88354bce80f3ed0d5b63a3ccfb12dffa577
Jojobusiness <johnfres.45@gmail.com>
2025-08-28 01:22:56 +0200

    Finalisation du site

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.',                 // <-- index.html reste à la racine
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
+  build: { outDir: 'dist', emptyOutDir: true }, // => dist (à la racine)
 });
